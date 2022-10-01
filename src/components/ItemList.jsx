31b4affe55d9af6b2cc4b84bec6948cfb15044f6
Santi <santiloureiro12@gmail.com>
2022-10-01 01:31:39 -0300

    added keyboard products and images

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -7,7 +7,7 @@ const ItemList = (props) => {
 
 
     return(
-        <div className="flex">
+        <div className="grid grid-cols-3">
             {props.products.map(item => <Item key={item.id} data={item} />)}
         </div>
     )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -16,32 +16,67 @@ const ItemListContainer = () => {
 useEffect(() => {
     const productos = [
         {   id: randomNumber(10000),
-            name: "Zapato",
-            description: "Esto es un zapato",
+            name: "Blueberry",
+            description: "TecladoDescrip",
             stock: 6,
             price: 0,
-            pictureUrl: "https://via.placeholder.com/150/#C1C1C1/808080?Text=Placeholder"
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284302/Blueberrysin_gbte2j.png"
         },
         {   id: randomNumber(10000),
-            name: "Gorra",
-            description: "Esto es una gorra",
+            name: "Strawberry",
+            description: "TecladoDescrip",
             stock: randomNumber(100),
             price: 0,
-            pictureUrl: "https://via.placeholder.com/150/#C1C1C1/808080?Text=Placeholder"
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/StrawBerrysin_ci4zo0.png"
         },
         {   id: randomNumber(10000),
-            name: "Remera",
-            description: "Esto es una remera",
+            name: "Yellow",
+            description: "TecladoDescrip",
             stock: randomNumber(100),
             price: 0,
-            pictureUrl: "https://via.placeholder.com/150/#C1C1C1/808080?Text=Placeholder"
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/Yellowsin_vna33d.png"
         },
         {   id: randomNumber(10000),
-            name: "Zapatilla",
-            description: "Esto es una zapatilla",
+            name: "OrangePeel",
+            description: "TecladoDescrip",
             stock: randomNumber(100),
             price: 0,
-            pictureUrl: "https://via.placeholder.com/150/#C1C1C1/808080?Text=Placeholder"
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284302/OrangePeel_Medium_lfcisa.png"
+        },
+        {   id: randomNumber(10000),
+            name: "Oreo",
+            description: "TecladoDescrip",
+            stock: randomNumber(100),
+            price: 0,
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284302/Oreo_Mediumsin_hov8av.png"
+        },
+        {   id: randomNumber(10000),
+            name: "BlueSky",
+            description: "TecladoDescrip",
+            stock: randomNumber(100),
+            price: 0,
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/BlueSky_Medium_vmfmdr.png"
+        },
+        {   id: randomNumber(10000),
+            name: "Bee",
+            description: "TecladoDescrip",
+            stock: randomNumber(100),
+            price: 0,
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/Bee_Largesin_gjxcoc.png"
+        },
+        {   id: randomNumber(10000),
+            name: "WaterMelon",
+            description: "TecladoDescrip",
+            stock: randomNumber(100),
+            price: 0,
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/WaterMelon_Large_cbe1wz.png"
+        },
+        {   id: randomNumber(10000),
+            name: "NightLight",
+            description: "TecladoDescrip",
+            stock: randomNumber(100),
+            price: 0,
+            pictureUrl: "https://res.cloudinary.com/djccefmjp/image/upload/v1654284301/NightLight_Largesin_y2xb8l.png"
         },
     ]
     new Promise((resolve, reject) => {
@@ -60,7 +95,7 @@ useEffect(() => {
             {loading ? (
                 <div className="h-96 bg-white">
                 <div className="flex justify-center items-center h-full">
-                <img className="h-10 w-10" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt=""></img>
+                <img className="h-10 w-10" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="TecladoDescrip"></img>
                 </div>
                 </div>
                 )
